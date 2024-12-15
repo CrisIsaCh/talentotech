d8109c049d396ec28ad35c4fff120a468792c31f
CristianICh <spikecarp@gmail.com>
2024-12-14 21:04:30 -0300

    modal

---
+++ js/market.js
@@ -0,0 +1,21 @@
+let abrir =document.getElementById('carrito');
+console.log(abrir);
+let modal =document.getElementById('modal-contenedor');
+console.log(modal);
+let toque =document.getElementsByClassName('carrito')
+console.log(toque);
+ let puf=document.querySelector('.carrito')
+ console.log(puf);
+ let mira =document.getElementById('product')
+ console.log(mira);
+ 
+ 
+
+
+
+abrir.addEventListener('click', (e)=>{
+
+    
+    
+     modal.classList.toggle('abrir')
+})
